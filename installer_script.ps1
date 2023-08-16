50c941ede281ee5159de585cdf9bde35ac06d7b5
Barnabas Miklos <barnabas.miklos@silabs.com>
2023-08-16 14:53:02 +0200

    added keysight io libraries requirement check

--- installer_script.ps1
+++ installer_script.ps1
@@ -8,17 +8,16 @@ $local_python_version = "3.10.5" # must be 3.x.x format
 
 $message_color = "DarkGreen"
 
-$keysight_visa_exec = "IOLibSuite_18_2_28014.exe"
-$keysight_visa_web_file = "\\silabs.com\design\home\bamiklos\IOLibSuite_18_2_28014.exe"
 #NI-VISA link,must contain .exe at the end can be switched to newer/older one
 $ni_visa_exec = "ni-visa_19.5_online_repack3.exe" #NI-VISA installer file name, can be switched to a newer/older one
 $ni_visa_web_file = "https://download.ni.com/support/nipkg/products/ni-v/ni-visa/19.5/online/ni-visa_19.5_online_repack3.exe" 
 
 function Print{
     param(
-        $string
+        $string,
+        $color = $message_color
     )
-    Write-Host $string -ForegroundColor $message_color
+    Write-Host $string -ForegroundColor $color
 }
 
 Print "Automated Measurement Framework Install Script" 
@@ -60,7 +59,7 @@ Print "Finished installing Python Packages"
 
 # downloading NI-VISA for all the instrument drivers
 Print "-------------------------------------------------------"
-$answer = Read-Host "Do you want to download and install NI-VISA (Y/N)"
+$answer = Read-Host "Do you want to download and install NI-VISA (required software) [Y/N]"
 if ($answer -eq "Y" -or $answer -eq "y") {
     Print "Fetching NI-VISA from the web ..."
     Start-BitsTransfer -Source $ni_visa_web_file -Destination ./$ni_visa_exec
@@ -74,45 +73,50 @@ if ($answer -eq "Y" -or $answer -eq "y") {
     Print "Skipping NI-VISA install...."
 }
 
-
-
 Print "-------------------------------------------------------"
-$answer = Read-Host "Do you want to download and install KEYSIGHT-VISA (Y/N)"
-if ($answer -eq "Y" -or $answer -eq "y") {
-    # User selected Yes
-    Print "Fetching KEYSIGHT-VISA from the SL network ... This might take a few minutes"
-    Start-BitsTransfer -Source $keysight_visa_web_file -Destination ./$keysight_visa_exec
-    Start-Process ("./$keysight_visa_exec")
-    Read-Host "If You Completed the KEYSIGHT-VISA Installer (or to skip) Press Enter"
-    Print "Removing installer file ..."
-    Remove-Item ("./$keysight_visa_exec")
-    Print "Installer file removed"
+$keysight_drivers_name = "Keysight IO Libraries"
+# searching for any version of Keysight IO Libraries
+$installedPackages = Get-Package | Where-Object { $_.Name -like "*$keysight_drivers_name*" }
+
+if ($installedPackages) {
+    Print "Found a version of $keysight_drivers_name installed."
+    Print "`t Make sure it is supported by AMF! " "Yellow"
+    Print "`t Currently tested versions:" "Yellow"
+    Print "`t --------------------------" "Yellow"
+    Print "`t - Keysight IO Libraries Suite 2022 Update 1" "Yellow"
+
 } else {
-    Print "Skipping KEYSIGHT-VISA install...."
+    Print "No version of $keysight_drivers_name found. Please install from this link: www.keysight.com/find/iosuite" "Red"
+    Print "IF NOT INSTALLED KEYSIGHT, AGILENT AND HP INSTRUMENTS WILL NOT FUNCTION PROPERLY!! " "Red"
+    Print "`t Currently tested versions:" "Yellow"
+    Print "`t --------------------------" "Yellow"
+    Print "`t - Keysight IO Libraries Suite 2022 Update 1, Download link: www.keysight.com/find/iosuite" "Yellow"
 }
 
 
 # updating the  submodules
 git submodule update --init --recursive
+
+
 # if compatibility mode is enabled
-if ($mode -eq "comp"){
-    $VersionFile = "versions.txt"
-    $Drivers = Get-Content $VersionFile | Out-String | ConvertFrom-StringData # read the driver version from versons.txt
-    Print "-------------------------------------------------------"
-    Print "Using these driver versions:"
-    Print $Drivers
-    foreach ($DriverName in $Drivers.Keys)
-    {
-        #change to submodule directory
-        Set-Location $DriverName
-        git config advice.detachedHead False #turn off detached head advice
-        Print $DriverName": "
-        git checkout $Drivers.$DriverName # checkout the compatible commit version
-        git config advice.detachedHead True #turn back detached head advice
-        Set-Location ..
+# if ($mode -eq "comp"){
+#     $VersionFile = "versions.txt"
+#     $Drivers = Get-Content $VersionFile | Out-String | ConvertFrom-StringData # read the driver version from versons.txt
+#     Print "-------------------------------------------------------"
+#     Print "Using these driver versions:"
+#     Print $Drivers
+#     foreach ($DriverName in $Drivers.Keys)
+#     {
+#         #change to submodule directory
+#         Set-Location $DriverName
+#         git config advice.detachedHead False #turn off detached head advice
+#         Print $DriverName": "
+#         git checkout $Drivers.$DriverName # checkout the compatible commit version
+#         git config advice.detachedHead True #turn back detached head advice
+#         Set-Location ..
         
-    }
-}
+#     }
+# }
 Print "===================Finished==================="
 Print "=============================================="
 Print "Virtual Environment active, to quit execute 'deactivate'"
